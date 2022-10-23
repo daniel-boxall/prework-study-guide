39d8d2de3f85a477e752fd40a969a45fb22eff27
Danny <danielboxall07@gmail.com>
2022-10-23 01:45:28 +0100

    added some notes to html

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,28 @@
 var topics = ['HTML', 'CSS', 'Git', 'Javascript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
 
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
 
-
-/*
-if (topic === 'HTML') {
-    console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-    console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-    console.lof("Let's study Git!");
-} else if (topic === 'Javascript') {
-    console.log("Let's study javascript!");
-} else {
-    console.log('Please try again!')
+function selectTopic() {
+    if (topics === 'HTML') {
+        console.log("Let's study HTML!");
+    } else if (topics === 'CSS') {
+        console.log("Let's study CSS!");
+    } else if (topics === 'Git') {
+        console.lof("Let's study Git!");
+    } else if (topics === 'Javascript') {
+        console.log("Let's study javascript!");
+    } else {
+        console.log('Please try again!')
+    }
 }
-*/
+
+console.log('Here are the topics we learned through Prework:' );
+listTopics();
+console.log('which topic should we study first?');
+selectTopic();
